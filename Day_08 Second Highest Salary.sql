7d8674860146f605e6506b9df0617a49d2450d39
Ishika Shaw <ishikashaw0207@gmail.com>
2026-03-30 00:02:11 +0530

    Leetcode 180

--- Day_08 Second Highest Salary.sql
+++
@@ -1,8 +0,0 @@
-with ranked_salary as(
-    select salary,
-        DENSE_RANK() over (order by salary desc) as rnk
-    from Employee
-)
-select max(salary) as SecondHighestSalary
-from ranked_salary
-where rnk = 2
