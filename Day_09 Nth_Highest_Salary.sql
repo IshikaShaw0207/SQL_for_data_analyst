575726642b709b27bc19a58ac31df9cc72fb65af
Ishika Shaw <146540309+IshikaShaw0207@users.noreply.github.com>
2026-03-30 00:00:37 +0530

    Leetcode 177

---
+++ Day_09 Nth_Highest_Salary.sql
@@ -0,0 +1,17 @@
+--Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+
+--Approach: CTE ranks salary using dense_rank() and final query filters the 2nd highest distinct salary
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      -- Write your MySQL query statement below.
+    with rankedsalary as(
+        select salary,
+            dense_rank() over (order by salary desc) as rnk
+            from Employee
+    )
+    select max(salary) from rankedsalary
+    where rnk=N
+  );
+END
