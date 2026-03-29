1d1902d388570129da4711f457d8e03e582dd89e
Ishika Shaw <146540309+IshikaShaw0207@users.noreply.github.com>
2026-03-30 00:43:53 +0530

    Update Day_08 Second_Highest_Salary.sql

--- Day_08 Second_Highest_Salary.sql
+++ Day_08 Second_Highest_Salary.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 --Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null.
 --Approach: CTE ranks salary using dense_rank() and final query filters the 2nd highest distinct salary
 
@@ -10,7 +9,6 @@ with ranked_salary as(
 )
 select max(salary) as SecondHighestSalary
 from ranked_salary
-=======
 with ranked_salary as(
     select salary,
         DENSE_RANK() over (order by salary desc) as rnk
@@ -18,5 +16,4 @@ with ranked_salary as(
 )
 select max(salary) as SecondHighestSalary
 from ranked_salary
->>>>>>> 8ce2d4360389d407d1ae13074544a852f53746e1
 where rnk = 2
