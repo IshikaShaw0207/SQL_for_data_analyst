ffe9f136dac883e8d5918740213110c0c094082f
Ishika Shaw <ishikashaw0207@gmail.com>
2026-04-15 21:08:54 +0530

    Leetcode 626

---
+++ Day_24_Exchange_Seats.sql
@@ -0,0 +1,11 @@
+-- Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped. Return the result table ordered by id in ascending order.
+
+# Write your MySQL query statement below
+SELECT
+CASE
+    WHEN id%2=1 AND id!=(SELECT MAX(id) FROM Seat) THEN id+1
+    WHEN id%2=0 THEN id-1
+    ELSE id
+END AS id, student
+FROM Seat
+ORDER BY id;
